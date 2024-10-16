9de5433dd42ece75fbdaf38b0c64aca4c8ce2d5e
daviniccacio <daviniccacio@gmail.com>
2024-10-16 18:44:54 -0300

    Tela de perfil

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { Login } from './screens/login-page/login';
+import { Profile } from './screens/profile/profile';
+
 
 export default function App() {
   return (
     <>
       <Login />
       <StatusBar style="auto" />
+      
     </>
   );
 }
--- screens/profile/profile.js
+++ screens/profile/profile.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
 
-const profile = () => {
-    return (
-        <div>profile</div>
-    )
+export function Profile() {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
 }
 
-export default profile
