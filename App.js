99a26944e3cc0cd54b8e19a2081d706cfedaffbb
6abrie1 <gabrieljv2019@gmail.com>
2024-10-24 15:12:32 -0300

    Adiciona lógica de admin na feature/Gabriel

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useState } from "react";
@@ -7,7 +7,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { View, StatusBar, StyleSheet } from "react-native";
 
 // pages
-import HomeScreen from "./src/screens/(auth)/home"
+import HomeScreen from "./src/screens/(auth)/home";
 import Financial from "./src/screens/(auth)/financial";
 import Profile from "./src/screens/(auth)/profile";
 import Training from "./src/screens/(auth)/workout";
@@ -38,19 +38,19 @@ const MyTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    background: 'transparent',  // Define o fundo transparente
+    background: "transparent", // Define o fundo transparente
   },
 };
 
 export default function App() {
   const [isSignedIn, setisSignedIn] = useState(true);
+  const [admin, setAdmin] = useState(false);
 
   const screenOptions = {
-
     animationEnabled: false,
     headerShown: false,
     tabBarStyle: {
-      position: 'absolute',
+      position: "absolute",
       backgroundColor: "#2B2B2B",
       height: 100,
       width: "90%",
@@ -58,7 +58,7 @@ export default function App() {
       marginBottom: 10,
       marginLeft: "5%",
       paddingBottom: 15,
-      borderTopWidth: 0,  // Remove a borda superior da navbar
+      borderTopWidth: 0, // Remove a borda superior da navbar
       elevation: 0, // Remove sombras em Android
       shadowOpacity: 0, // Remove sombras no iOS
     },
@@ -70,14 +70,11 @@ export default function App() {
     },
     tabBarActiveTintColor: "#ED5359",
     tabBarInactiveTintColor: "white",
-
   };
 
   return (
-
     <View style={styles.appContainer}>
-      <NavigationContainer theme={MyTheme} >
-
+      <NavigationContainer theme={MyTheme}>
         <StatusBar
           animated={true}
           translucent
@@ -88,8 +85,7 @@ export default function App() {
         {!isSignedIn ? (
           <Login login={() => setisSignedIn(true)} />
         ) : (
-
-          <Tab.Navigator screenOptions={screenOptions}  >
+          <Tab.Navigator screenOptions={screenOptions}>
             <Tab.Screen
               name="Menu"
               component={HomeStack}
@@ -120,29 +116,28 @@ export default function App() {
                 ),
               }}
             />
-            <Tab.Screen
-              name="Perfil"
-              component={Profile}
-              options={{
-                headerShown: false,
-                tabBarIcon: ({ color, size }) => (
-                  <Ionicons name="person" color={color} size={size} />
-                ),
-              }}
-            />
-
+            {admin ? (
+              <Tab.Screen
+                name="Perfil"
+                component={Profile}
+                options={{
+                  headerShown: false,
+                  tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="person" color={color} size={size} />
+                  ),
+                }}
+              />
+            ) : null}
           </Tab.Navigator>
         )}
-
-
       </NavigationContainer>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    backgroundColor: 'transparent', // Define como transparente o container da aplicação
+    backgroundColor: "transparent", // Define como transparente o container da aplicação
   },
-})
+});
