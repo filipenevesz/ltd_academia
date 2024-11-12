d55a13bb61296b34e185532fd11b6c9062398b56
Andre-MB <andrezinhogame15@gmail.com>
2024-11-12 20:28:35 -0300

    fetch de algumas informaçoes do perfil

--- src/screens/(auth)/(aluno)/profile/index.js
+++ src/screens/(auth)/(aluno)/profile/index.js
@@ -4,8 +4,62 @@ import { Ionicons } from "@expo/vector-icons";
 import React from 'react'
 import Button from '../../../../components/button';
 import Header from '../../../../components/header';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from "axios";
+import { useState, useEffect } from 'react';
+
+
 
 export default function Profile() {
+  const [cpf, setCpf] = useState('');
+  const [email, setEmail] = useState('');
+  const [role, setRole] = useState('');
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+  const [createdAt, setCreatedAt] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = await AsyncStorage.getItem('token');
+
+        if (!token) {
+          throw new Error('Token não encontrado');
+        }
+
+        const response = await axios.get('http://34.71.4.173:8081/auth/me', {
+          headers: {
+            Authorization: `Bearer ${token}`, // Passando o token no cabeçalho
+          },
+        });
+
+        const data = response.data;
+
+        // Definindo os estados com os valores da resposta
+        setCpf(data.cpf);
+        setEmail(data.email);
+        setRole(data.role);
+        setName(data.name);
+        setLastName(data.lastName);
+        setBirthDate(data.birthDate);
+        setCreatedAt(data.createdAt);
+        setImageUrl(data.imageUrl);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  console.log(imageUrl)
+
   return (
     <SafeAreaView style={styles.container}>
       <Header
@@ -16,13 +70,18 @@ export default function Profile() {
         <View style={styles.profile}>
           <View style={styles.profileContainer}>
             <View style={styles.profilePhoto}>
-              <Ionicons name="person-circle" size={120} color={"#ffffff"} />
+              {/* <Ionicons name="person-circle" size={120} color={"#ffffff"} /> */}
+
+              <Image
+                source={{ uri: imageUrl }}
+                style={{ width: 120, height: 120, borderRadius: 60 }}
+              />
             </View>
             <TouchableOpacity style={styles.profilePhotoChange}>
               <Ionicons name="camera-reverse" size={20} color={"#ffffff"} />
             </TouchableOpacity>
           </View>
-          <Text style={styles.profileTextName}>Francisco Pinto</Text>
+          <Text style={styles.profileTextName}>{name} {lastName}</Text>
           <View style={styles.addressContainer}>
             <Ionicons name="call" size={15} color={"#2B2B2B"} />
             <Text> (99) 9 9999-9999</Text>
@@ -37,7 +96,7 @@ export default function Profile() {
           <Text style={styles.textInfo}>Minhas informações:</Text>
           <ScrollView style={styles.card}>
             <Text style={styles.textCard}>
-              Email: <Text style={styles.textCardInfo}>daviniccacio@gmail.com</Text>
+              Email: <Text style={styles.textCardInfo}>{email}</Text>
             </Text>
             <Text style={styles.textCard}>
               Endereço: <Text style={styles.textCardInfo}>Rua Argentina, 98, Centro</Text>
@@ -57,11 +116,11 @@ export default function Profile() {
         <View style={styles.buttons}>
           <Button
             title='Editar Perfil'
-            onPress = {()=>alert("CRIAR FUNCIONALIDADE DE VER PERFIL")}
+            onPress={() => alert("CRIAR FUNCIONALIDADE DE VER PERFIL")}
           />
           <Button
             title='Ver Ficha'
-            onPress = {()=>alert("CRIAR FUNCIONALIDADE DE VER FICHA")}
+            onPress={() => alert("CRIAR FUNCIONALIDADE DE VER FICHA")}
           />
         </View>
       </ScrollView>
--- src/services/api.js
+++ src/services/api.js
@@ -1,42 +1,41 @@
 import axios from "axios";
 import { getToken } from "./AuthService";
-const api = axios.create({
-    baseURL: "http://34.71.4.173:8081",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    
 
+const api = axios.create({
+  baseURL: "http://34.71.4.173:8081",
+  headers: {
+    "Content-Type": "application/json",
+  },
 })
 api.interceptors.request.use(
-    async (config) => {
-      const token = await getToken();
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-
-      return config;
-    },
-    (error) => {
-      console.log("Error in request interceptor", error);
-      return Promise.reject(error);
+  async (config) => {
+    const token = await getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-  );
 
-  api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      if (error.response) {
-        console.error("Erro na resposta:", error.response.data);
-        console.error("Status:", error.response.status);
-        console.error("Headers:", error.response.headers);
-      } else if (error.request) {
-        console.error("Erro na requisição:", error.request);
-      } else {
-        console.error("Erro ao configurar a requisição:", error.message);
-      }
-      return Promise.reject(error);
+    return config;
+  },
+  (error) => {
+    console.log("Error in request interceptor", error);
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      console.error("Erro na resposta:", error.response.data);
+      console.error("Status:", error.response.status);
+      console.error("Headers:", error.response.headers);
+    } else if (error.request) {
+      console.error("Erro na requisição:", error.request);
+    } else {
+      console.error("Erro ao configurar a requisição:", error.message);
     }
-  );
+    return Promise.reject(error);
+  }
+);
 
 export default api;
